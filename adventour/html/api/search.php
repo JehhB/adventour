a1e8d068d61c28e1035cbe4eb16ee135fe3a0596
JehhB <jwy.bayod@gmail.com>
2023-05-09 22:44:37 +0800

    Modified sql statemnt for fetching search suggestion

--- adventour/html/api/search.php
+++ adventour/html/api/search.php
@@ -3,24 +3,28 @@ include $_SERVER['DOCUMENT_ROOT'] . '/../include.php';
 global $conn;
 
 $sql = <<<SQL
-SELECT 
-  MIN(Hotels.hotel_id) AS hotel_id,
-  MIN(hotel_image_id) AS image_id,
-  MIN(name) AS name, 
-  MIN(address) AS address,
-  MIN(description) as description
-FROM Hotels LEFT JOIN HotelImages
-ON HotelImages.hotel_id = Hotels.hotel_id
-WHERE caption != ''
+SELECT
+    Hotels.hotel_id hotel_id,
+    hotel_image_id AS image_id,
+    caption,
+    name,
+    address,
+    description
+FROM
+    Hotels
+LEFT JOIN HotelImages ON HotelImages.hotel_id = Hotels.hotel_id
+WHERE hotel_image_id = (
+    SELECT MIN(hotel_image_id)
+    FROM HotelImages
+    WHERE caption != '' AND HotelImages.hotel_id = Hotels.hotel_id
+  )
 SQL;
 
 if (isset($_GET['q']) && !empty($_GET['q'])) {
   $sql .= " AND metaphone LIKE :q";
 }
 $sql .= <<<SQL
-
-GROUP BY Hotels.hotel_id
-LIMIT 8
+  LIMIT 8
 SQL;
 
 $stmt = $conn->prepare($sql);
@@ -40,9 +44,9 @@ $results = $stmt->fetchAll();
   foreach ($results as $result) {
       insert('search-summary', [
         'isLoading' => false,
-        'link' => "/hotels.php?hotel_id={$result['hotel_id']}",
-        'image' => "/assets/hotelImage.php?hotel_image_id={$result['image_id']}",
-        'alt' => "Image of {$result['name']}",
+        'link' => "/hotel.php?hotel_id={$result['hotel_id']}",
+        'image' => "/assets/images/hotelImage.php?hotel_image_id={$result['image_id']}",
+        'alt' => $result['caption'],
         'name' => $result['name'],
         'address' => $result['address'],
       ]); 
