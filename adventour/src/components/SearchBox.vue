54c373c777a0691c84f59b7b14b25df275de77ed
JehhB <jwy.bayod@gmail.com>
2023-05-24 20:34:29 +0800

    Added location for search siggestion

--- adventour/src/components/SearchBox.vue
+++ adventour/src/components/SearchBox.vue
@@ -1,7 +1,11 @@
 <template>
   <div
-    class="absolute border-gray-400 px-4 py-1 bg-white z-40 flex items-center transition-all duration-[50ms] sm:relative sm:inset-0 sm:w-[40ch] sm:rounded-2xl sm:border"
-    :class="isActive ? 'inset-0 sm:border-b-0 sm:rounded-b-none' : 'inset-x-14 inset-y-2 rounded-xl border'"
+    class="search-box absolute border-gray-400 px-4 py-1 bg-white z-40 flex items-center transition-[left,right,bottum,top,border-radius] duration-[50ms] sm:relative sm:inset-0 sm:w-[40ch] sm:rounded-2xl sm:border sm:transition-[border-radius]"
+    :class="
+      isActive
+        ? 'inset-0 sm:border-b-0 sm:rounded-b-none'
+        : 'inset-x-14 inset-y-2 rounded-xl border delay-[250ms]'
+    "
   >
     <form
       action="/search.php"
@@ -18,8 +22,19 @@
         class="flex-1 w-0 placeholder:text-gray-500"
       />
       <BIconSearch v-if="!isActive" class="text-gray-500" />
-      <BIconXLg v-else="!isActive" @click="clear" class="text-gray-500" />
+      <BIconXLg v-else @click="clear" class="text-gray-500" />
     </form>
+    <Transition
+      enter-from-class="max-h-0"
+      enter-to-class="transition-all delay-[50ms] duration-[250ms] max-h-screen"
+      leave-from-class="max-h-screen"
+      leave-to-class="transition-all duration-[250ms] max-h-0"
+    >
+      <div
+        v-show="isActive"
+        class="absolute inset-x-0 top-full h-[calc(100vh_-_100%)] bg-white sm:inset-x-[-1px] sm:h-[400px] sm:border sm:border-t-0 sm:border-gray-400 sm:rounded-b-2xl"
+      ></div>
+    </Transition>
   </div>
 </template>
 <script setup lang="ts">
@@ -28,9 +43,9 @@ import { reactive, ref } from "vue";
 
 const isActive = ref(false);
 const query = reactive({
-  search: '',
-  filter: 'all',
-})
+  search: "",
+  filter: "all",
+});
 
 function onFocus(event: FocusEvent) {
   (event.target as HTMLInputElement).select();
@@ -38,7 +53,7 @@ function onFocus(event: FocusEvent) {
 }
 
 function clear() {
-  query.search = '';
+  query.search = "";
   isActive.value = false;
 }
 </script>
