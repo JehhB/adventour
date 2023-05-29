142ce04bf02564862dd5aa39783d403fed11dcc3
JehhB <jwy.bayod@gmail.com>
2023-05-29 09:56:07 +0800

    Added minifications of php view files

--- adventour/vue.config.js
+++ adventour/vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
+const { minify } = require("html-minifier");
+
 module.exports = defineConfig({
   pages: {
     index: {
@@ -17,6 +19,30 @@ module.exports = defineConfig({
       filename: "dist/js/[name].[contenthash:8].js",
       chunkFilename: "dist/js/[name].[contenthash:8].js",
     },
+  },
+  chainWebpack: (config) => {
+    config.plugin("copy").tap((args) => {
+      args[0].patterns[0].transform = (input, path) => {
+        if (
+          process.env.NODE_ENV === "production" &&
+          /lib\/views\/.*?\.php$/.test(path)
+        ) {
+          return minify(input.toString(), {
+            collapseWhitespace: true,
+            minifyCSS: true,
+            minifyJS: true,
+            removeComments: true,
+            continueOnParseError: true,
+            sortAttribues: true,
+            sortClassName: true,
+            removeRedundantAttributes: true,
+          });
+        }
+        return input;
+      };
+
+      return args;
+    });
   },
   css: {
     extract: true,
