b5c1976ba0d33ead8aec00e20e4807112f1fd9cb
JehhB <jwy.bayod@gmail.com>
2023-06-04 17:44:41 +0800

    Made hotel overview responsive

--- adventour/public/lib/views/hotel-overview.php
+++ adventour/public/lib/views/hotel-overview.php
@@ -7,16 +7,22 @@ WHERE hotel_id = ?
 SQL;
 $stmt = execute($sql, [$hotel_id]);
 ?>
-<section class="mt-3 space-y-2">
-  <div class="flex items-center gap-2 px-2 sm:px-0">
-    <h1 class="font-semibold text-neutral-800"><?= $name ?></h1>
+<section
+  class="md:grid-rows-overview mt-3 space-y-2 md:grid md:grid-cols-2 md:gap-x-4 md:space-y-0 xl:gap-x-16 2xl:gap-x-20"
+>
+  <div class="flex h-6 items-center gap-2 px-2 sm:px-0 md:col-start-2">
+    <h1 class="font-semibold leading-none text-neutral-800"><?= $name ?></h1>
     <div class="ml-auto flex gap-2">
       <b-icon-heart-fill class="stroke-2 text-zinc-600"></b-icon-heart-fill>
       <b-icon-share-fill class="stroke-2 text-zinc-600"></b-icon-share-fill>
     </div>
   </div>
+
   <h2 class="sr-only">Hotel overview</h2>
-  <gallery-container>
+
+  <gallery-container
+    class="md:col-start-1 md:row-span-4 md:row-start-1 md:self-center"
+  >
     <?php foreach ($images as $image) : ?>
     <gallery-item
       src="<?= $image['src'] ?>"
@@ -25,20 +31,34 @@ $stmt = execute($sql, [$hotel_id]);
     <?php endforeach; ?>
   </gallery-container>
 
-  <div class="px-2">
-    <h3 class="text-base font-semibold leading-none text-gray-800">
+  <address class="hidden text-sm leading-none text-gray-800 md:block">
+    <?= $address ?>
+  </address>
+
+  <div class="hidden items-center py-2 md:flex">
+    <p class="text-sm leading-none text-gray-800">
+      <?= $description ?>
+    </p>
+  </div>
+
+  <div
+    class="px-2 md:flex md:items-center md:border-t md:border-green-900 md:px-0 md:py-4"
+  >
+    <h3 class="text-base font-semibold leading-none text-gray-800 md:hidden">
       Facilities
     </h3>
-    <ul class="mt-4 flex list-none flex-wrap gap-x-8 gap-y-3 text-gray-700">
+    <ul
+      class="mt-4 flex list-none flex-wrap gap-x-8 gap-y-3 text-gray-700 md:mt-0"
+    >
       <?php while ($facility = $stmt->fetch()) : ?>
-      <li class="list-check text-base leading-none">
+      <li class="list-check text-sm leading-none">
         <?= e($facility['feature']) ?>
       </li>
       <?php endwhile; ?>
     </ul>
   </div>
 
-  <div class="px-2">
+  <div class="px-2 md:hidden">
     <toggle-container>
       <open-button class="font-bold text-green-900">Learn more</open-button>
       <template v-slot:toggled>
