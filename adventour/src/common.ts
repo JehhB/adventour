18f6fc8620afe4c3139b0f843ecbcf0c1782f7da
JehhB <jwy.bayod@gmail.com>
2023-06-13 19:57:49 +0800

    Added popover container

--- adventour/src/common.ts
+++ adventour/src/common.ts
@@ -4,6 +4,7 @@ import OffPage from "./components/OffPage.vue";
 import OpenButton from "./components/OpenButton.vue";
 import SearchBox from "./components/SearchBox.vue";
 import ToastContainer from "./components/ToastContainer.vue";
+import PopoverContainer from "./components/PopoverContainer.vue";
 
 import { BIconList } from "bootstrap-icons-vue";
 import type { App } from "vue";
@@ -13,6 +14,7 @@ export default {
     app.component("BIconList", BIconList);
     app.component("OffPage", OffPage);
     app.component("OpenButton", OpenButton);
+    app.component("PopoverContainer", PopoverContainer);
     app.component("SearchBox", SearchBox);
     app.component("ToastContainer", ToastContainer);
   },
---
+++ adventour/src/components/PopoverContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    v-show="toggleable.active.value"
+    class="absolute right-0 top-full drop-shadow-lg filter"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      viewBox="0 0 16 8"
+      class="absolute right-5 top-0 h-2 w-4 translate-x-1/2 sm:right-6"
+    >
+      <polygon fill="white" style="" points="0,8 16,8 8,0" />
+    </svg>
+    <OnClickOutside
+      @trigger="toggleable.close()"
+      class="absolute right-0 top-2 min-w-[48px] rounded-lg bg-white"
+    >
+      <slot></slot>
+    </OnClickOutside>
+  </div>
+</template>
+<script setup lang="ts">
+import { defineProps } from "vue";
+import { useToggleable } from "../util";
+import { toggleables } from "../stores";
+import { OnClickOutside } from "@vueuse/components";
+
+const props = defineProps<{
+  name: string | symbol;
+}>();
+
+const toggleable = useToggleable(true);
+toggleables.set(props.name, toggleable);
+</script>
